e221de8713317f8db1f9b81f2c96c2effa4e49b3
David Poisson <zeraaiur@hotmail.com>
2018-05-21 23:33:56 -0400

    add ingredient v2

--- app/javascript/components/new_dose.js
+++ app/javascript/components/new_dose.js
@@ -2,32 +2,42 @@ function displayNothing(){
 document.querySelectorAll(".form-class").forEach(function(element) {
   element.classList.add("display-none");
 });
+removeButtonActive()
 }
+function removeButtonActive(){
+document.querySelectorAll(".btn-form-display").forEach(function(element) {
+  element.classList.remove("btn-active");
+});
+}
+
 function displayGoodform(title){
 document.getElementById(title.toLowerCase()+"-form").classList.remove("display-none");
 }
 
 document.getElementById("alcohol-btn").addEventListener("click", function clickBtnAlcohol( event ) {
-    // Affiche le compte courant de clics à l'intérieur de la div cliquée
 displayNothing();
-displayGoodform(this.innerHTML)
+this.classList.add("btn-active");
+displayGoodform(this.value)
   }, false);
+
+
+
 document.getElementById("juice-btn").addEventListener("click", function clickBtnAlcohol( event ) {
-    // Affiche le compte courant de clics à l'intérieur de la div cliquée
 displayNothing();
-displayGoodform(this.innerHTML)
+this.classList.add("btn-active");
+displayGoodform(this.value)
   }, false);
 
 document.getElementById("spice-btn").addEventListener("click", function clickBtnAlcohol( event ) {
-    // Affiche le compte courant de clics à l'intérieur de la div cliquée
 displayNothing();
-displayGoodform(this.innerHTML)
+this.classList.add("btn-active");
+displayGoodform(this.value)
   }, false);
 
 document.getElementById("food-btn").addEventListener("click", function clickBtnAlcohol( event ) {
-    // Affiche le compte courant de clics à l'intérieur de la div cliquée
 displayNothing();
-displayGoodform(this.innerHTML)
+this.classList.add("btn-active");
+displayGoodform(this.value)
   }, false);
 
 
